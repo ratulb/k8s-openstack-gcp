6172cabb21f5e89cee8facedbf5f5fba11470032
Ratul Buragohain <ratul75@hotmail.com>
2020-11-18 19:50:14 +0000

    Color indication for commands

--- launch.sh
+++ launch.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 #Setup kubernetes cluster on openstack deployed on google compute engine
-CUREENT=$(pwd)
+RED='\033[0;31m'
+
 . create-stack-user.sh
 . repo-clone.sh
 . gen-conf.sh
 cp $CURRENT/dev-tenant-setup.sh /opt/stack/devstack/
 chmod +x /opt/stack/devstack/dev-tenant-setup.sh
 chown -R stack:stack /opt/stack/
-echo "Please cd devstack and run"
-echo "./stack.sh"
-echo "./dev-tenant-setup.sh"
+printf "${RED}Please run the following\n"
+printf "${RED}cd devstack\n"
+printf  "${RED}./stack.sh\n"
+printf "${RED}./dev-tenant-setup.sh\n"
 su - stack
 
